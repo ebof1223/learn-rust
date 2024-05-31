4d49f6334ea6d71e96c901ab4085008f3fb819bd
ebof1223 <erwinjbofetiado@gmail.com>
2024-05-31 13:26:50 -0500

    linkedlist test

--- data-structures/src/lib.rs
+++ data-structures/src/lib.rs
@@ -1,17 +1,28 @@
 mod binary_rev;
 mod linkedlist;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn linked_list_int() {
+        let mut list: linkedlist::LinkedList<i32> = linkedlist::LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        list.push_back(4);
+        list.push_back(5);
+
+        let head = list.head.unwrap();
+        assert_eq!(head.val, 1);
+        let mut node = head.next.unwrap();
+        assert_eq!(node.val, 2);
+        let mut count: i32 = 3;
+        while let Some(current) = node.next {
+            assert_eq!(current.val, count);
+            count += 1;
+            node = current;
+        }
     }
 }
--- data-structures/src/linkedlist.rs
+++ data-structures/src/linkedlist.rs
@@ -1,23 +1,23 @@
 pub struct Node<T> {
-    val: T,
-    next: Option<Box<Node<T>>>,
+    pub val: T,
+    pub next: Option<Box<Node<T>>>,
 }
 
 pub struct LinkedList<T> {
-    head: Option<Box<Node<T>>>,
+    pub head: Option<Box<Node<T>>>,
 }
 
 impl<T> Node<T> {
-    fn new(val: T) -> Self {
+    pub fn new(val: T) -> Self {
         Node { val, next: None }
     }
 }
 
 impl<T> LinkedList<T> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         LinkedList { head: None }
     }
-    fn push_back(&mut self, val: T) {
+    pub fn push_back(&mut self, val: T) {
         let node = Some(Box::new(Node::new(val)));
         if self.head.is_none() {
             self.head = node;
@@ -32,13 +32,21 @@ impl<T> LinkedList<T> {
     }
 }
 
-fn vec_to_linkedlist<T>(vector: Vec<T>) -> LinkedList<T> {
+pub fn vec_to_linkedlist<T>(vector: Vec<T>) -> LinkedList<T> {
     let mut list: LinkedList<T> = LinkedList::new();
     let mut vector = vector.into_iter();
-
     while let Some(item) = vector.next() {
         list.push_back(item)
     }
-
     list
+}
+
+pub fn linkedlist_to_vec<T>(list: LinkedList<T>) -> Vec<T> {
+    let mut vector = vec![];
+    let mut node = list.head;
+    while let Some(current) = node {
+        vector.push(current.val);
+        node = current.next;
+    }
+    vector
 }
