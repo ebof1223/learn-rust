dfb38c230a4c0bab9371b01c8d036a4c898d63f4
= <erwinjbofetiado@gmail.com>
2024-06-03 11:10:16 -0500

    pop

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,5 +1,33 @@
+use std::mem;
+
 pub struct List {
     head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
+    pub fn pop(&mut self) -> Option<i32> {
+        let result;
+        match &self.head {
+            Link::Empty => {
+                result = None;
+            }
+            Link::More(node) => {
+                result = Some(node.elem);
+                self.head = node.next
+            }
+        };
+        unimplemented!()
+    }
 }
 
 enum Link {
