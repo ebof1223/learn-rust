262533256744162bf90ece5e8b5600109d60d2df
= <erwinjbofetiado@gmail.com>
2024-06-03 14:37:42 -0500

    drop

--- data_structures/target/debug/deps/data_structures-1164b94de07f61c9.d
+++ data_structures/target/debug/deps/data_structures-1164b94de07f61c9.d
@@ -1,7 +1,7 @@
-/home/bofetiad/Code/tuts/learn-rust/data-structures/target/debug/deps/libdata_structures-1164b94de07f61c9.rmeta: src/lib.rs src/binary_rev.rs src/linkedlist.rs
+/home/bofetiad/Code/tuts/learn-rust/data_structures/target/debug/deps/libdata_structures-1164b94de07f61c9.rmeta: src/lib.rs src/dbllinkedlist.rs src/linkedlist.rs
 
-/home/bofetiad/Code/tuts/learn-rust/data-structures/target/debug/deps/data_structures-1164b94de07f61c9.d: src/lib.rs src/binary_rev.rs src/linkedlist.rs
+/home/bofetiad/Code/tuts/learn-rust/data_structures/target/debug/deps/data_structures-1164b94de07f61c9.d: src/lib.rs src/dbllinkedlist.rs src/linkedlist.rs
 
 src/lib.rs:
-src/binary_rev.rs:
+src/dbllinkedlist.rs:
 src/linkedlist.rs:
---
+++ data_structures/target/debug/deps/linkedlist-5a5bca8ad272f7c3.d
@@ -0,0 +1,5 @@
+/home/bofetiad/Code/tuts/learn-rust/data_structures/target/debug/deps/liblinkedlist-5a5bca8ad272f7c3.rmeta: tests/linkedlist.rs
+
+/home/bofetiad/Code/tuts/learn-rust/data_structures/target/debug/deps/linkedlist-5a5bca8ad272f7c3.d: tests/linkedlist.rs
+
+tests/linkedlist.rs:
