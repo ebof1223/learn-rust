62adb13e22a805fbc2c6bd3d6650c44502577d7b
= <erwinjbofetiado@gmail.com>
2024-06-02 22:03:37 -0500

    lists

---
+++ lists/target/debug/deps/lists-064289c2bfe312b4.d
@@ -0,0 +1,6 @@
+/home/bofetiad/Code/tuts/learn-rust/lists/target/debug/deps/liblists-064289c2bfe312b4.rmeta: src/lib.rs src/first.rs
+
+/home/bofetiad/Code/tuts/learn-rust/lists/target/debug/deps/lists-064289c2bfe312b4.d: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
---
+++ lists/target/debug/deps/lists-20f67bdc2e9737ff.d
@@ -0,0 +1,6 @@
+/home/bofetiad/Code/tuts/learn-rust/lists/target/debug/deps/liblists-20f67bdc2e9737ff.rmeta: src/lib.rs src/first.rs
+
+/home/bofetiad/Code/tuts/learn-rust/lists/target/debug/deps/lists-20f67bdc2e9737ff.d: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
