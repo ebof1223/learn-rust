1a6b894b3c6165fe07296a184314235366119388
ebof1223 <erwinjbofetiado@gmail.com>
2024-06-26 13:46:05 -0500

    17 init

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,21 +1,30 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
-
-fn main() {
-    let counter = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
-
-            *num += 1;
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-    println!("Result: {}", *counter.lock().unwrap());
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
 }
+
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value)
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        if let Some(value) = self.list.pop() {
+            self.update_average();
+            Some(value)
+        } else {
+            None
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let sum: i32 = self.list.iter().sum();
+        self.average = sum as f64 / self.list.len() as f64;
+    }
+}
+
+fn main() {}
